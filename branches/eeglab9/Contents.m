4215dfe808c9a1c2ca6d34461d618e0bbe9518cd
arno <arno@82734b14-a7fd-429e-7580-97e0576a5d90>
2011-08-27 21:59:17 +0000

    add revision

--- branches/eeglab9/Contents.m
+++ branches/eeglab9/Contents.m
@@ -1,5 +1,5 @@
 % EEGLAB Toolbox to process EEG data
-% Version 9.0.6.6b
+% Version 9.0.7.6b
 %
 % eeglab() - Matlab graphic user interface environment for 
 %   electrophysiological data analysis incorporating the ICA/EEG toolbox 
